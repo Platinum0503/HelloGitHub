37609647a271197d2baeb6be0717501ac5715435
Dang <proae191@gmail.com>
2026-01-07 10:06:19 +0700

    test getChar

---
+++ getChar.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    char str[50];
+
+    printf("Nhap so: ");
+    scanf("%d", &n);
+    while(getchar() != '\n'); //caution to clear buffer
+
+    printf("nhap chuoi: ");
+    fgets(str, 50, stdin); 
+    
+    printf("ket qua: %s\n", str);
+
+}
